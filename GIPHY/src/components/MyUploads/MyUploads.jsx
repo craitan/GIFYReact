e21b671280e46055a1b119775f93ab8b16dd60a8
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-01 12:12:23 +0200

    created the components that we need for now

---
+++ GIPHY/src/components/MyUploads/MyUploads.jsx
@@ -0,0 +1,9 @@
+const MyUploads = () => {
+    return (
+        <div>
+            <h1>MyUploads</h1>
+        </div>
+    );
+};
+
+export default MyUploads;
--- GIPHY/src/components/NavBar/NavBar.jsx
+++ GIPHY/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,8 @@
-const NavBar = () => {
+import { NavLink } from 'react-router-dom';
 
+
+const NavBar = () => {
+    <NavLink to="/">Trending</NavLink>
 }
 
 export default NavBar;
