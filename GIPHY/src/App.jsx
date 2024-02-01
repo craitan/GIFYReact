96d82081a6901d9e5e201832fdf00ceafc64a35c
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-01 12:24:36 +0200

    created routes for the components

--- GIPHY/src/App.jsx
+++ GIPHY/src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Trending from './components/Trending/Trending'
+import Favorites from './components/Favorites/Favorites'
+import MyUploads from './components/MyUploads/MyUploads'
+import Upload from './components/Upload/Upload'
 
 function App() {
 
   return (
     <>
-    <Header />
-    <Footer />
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/trending" />} />
+          <Route path="trending" element={<Trending />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="upload" element={<Upload />} />
+          <Route path="myuploads" element={<MyUploads />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
   )
 }
